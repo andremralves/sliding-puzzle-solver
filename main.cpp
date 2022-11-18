67a2b9d2df49a60ee884f53d15ca3267e064abe0
Andre Alves <andremralves@gmail.com>
2022-11-18 08:49:55 -0300

    update algorithm to use custom row/col sizes

--- main.cpp
+++ main.cpp
@@ -1,29 +1,58 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <chrono>
 
 class Solver
 {
 private:
     bool found = false;
+    int rows = 3;
+    int cols = 3;
     std::string ans = "012345678";
     std::vector<std::string> history;
 public:
+    Solver() {}
+    Solver(int r, int c) {
+        rows = r; 
+        cols = c;
+    }
+
     void printHistory() {
         int step = 0;
         for(int i=history.size()-1; i>= 0; i--) {
             if(i == history.size()-1) 
-                std::cout<<"Estado inicial:"<<std::endl;
+                std::cout<<"Initial State:"<<std::endl;
             else
-                std::cout<<"Passo: "<<step<<std::endl; step++;
+                std::cout<<"Step: "<<step<<std::endl; step++;
             std::vector<std::string> rows = splitStringIntoRows(history[i]);
             for(std::string row : rows) {
                 std::cout<<row<<"\n";
             }
             std::cout<<"\n";
         }
+        std::cout<<"Total steps: "<<getHistorySize()-1<<std::endl;
     }
+
+    void solve(std::string iniState) {
+        k_limited(iniState); 
+        printHistory();
+    }
+
+    void k_limited(std::string iniState) {
+        for (int k = 0; k < 25; k++) {
+            if(getFound() == true) break;
+            int initialState[3][3] = {
+                {0, 1, 2},
+                {3, 4, 5},
+                {6, 7, 8},
+            };
+            dfs(iniState, 0, k);
+        }
+    }
+
     void dfs(std::string state, int cnt, int k) {
+        //std::cout<<"ok dfs"<<"\n";
         if(found == true) return;
         if(cnt >= k) {
             //printHistory();
@@ -62,43 +91,67 @@ public:
         return rows;
     }
 
+    std::vector<std::vector<char>> getMatrixFromString(std::string state) {
+        std::vector<std::vector<char>> matrix(rows, std::vector<char>(cols));
+        state[0];
+        int si = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = state[si++];
+            }
+        }
+        return matrix;
+    }
+
+    std::string getStringFromMatrix(std::vector<std::vector<char>> matrix) {
+        std::string strMatrix = "";
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                strMatrix += matrix[i][j];
+            }
+        }
+        return strMatrix;
+    }
+
     std::vector<std::string> getPossibleMovments(std::string currentState) {
+        //std::cout<<"ok mov"<<"\n";
         std::vector<std::string> possibleMovments;
-        std::vector<std::string> rows = splitStringIntoRows(currentState);
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < rows[0].length(); j++) {
-                if(rows[i][j] == '0') {
+        std::vector<std::vector<char>> matrix = getMatrixFromString(currentState);
+        //std::cout<<"ok mov 2"<<"\n";
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if(matrix[i][j] == '0') {
                     if(i+1 < 3) {
-                        std::vector<std::string> newRows = rows;
-                        int buff = newRows[i][j];
-                        newRows[i][j] = newRows[i+1][j];
-                        newRows[i+1][j] = buff;
-                        std::string rowsString = appendRowsIntoString(newRows);
+                        std::vector<std::vector<char>> newMatrix = matrix;
+                        int buff = newMatrix[i][j];
+                        newMatrix[i][j] = newMatrix[i+1][j];
+                        newMatrix[i+1][j] = buff;
+                        std::string rowsString = getStringFromMatrix(newMatrix);
                         possibleMovments.push_back(rowsString);
                     }
                     if(i-1 >= 0) {
-                        std::vector<std::string> newRows = rows;
-                        int buff = newRows[i][j];
-                        newRows[i][j] = newRows[i-1][j];
-                        newRows[i-1][j] = buff;
-                        std::string rowsString = appendRowsIntoString(newRows);
-                        possibleMovments.push_back(rowsString);
+                        std::vector<std::vector<char>> newMatrix = matrix;
+                        int buff = newMatrix[i][j];
+                        newMatrix[i][j] = newMatrix[i-1][j];
+                        newMatrix[i-1][j] = buff;
+                        std::string strMatrix = getStringFromMatrix(newMatrix);
+                        possibleMovments.push_back(strMatrix);
                     }
                     if(j+1 < 3) {
-                        std::vector<std::string> newRows = rows;
-                        int buff = newRows[i][j];
-                        newRows[i][j] = newRows[i][j+1];
-                        newRows[i][j+1] = buff;
-                        std::string rowsString = appendRowsIntoString(newRows);
-                        possibleMovments.push_back(rowsString);
+                        std::vector<std::vector<char>> newMatrix = matrix;
+                        int buff = newMatrix[i][j];
+                        newMatrix[i][j] = newMatrix[i][j+1];
+                        newMatrix[i][j+1] = buff;
+                        std::string strMatrix = getStringFromMatrix(newMatrix);
+                        possibleMovments.push_back(strMatrix);
                     }
                     if(j-1 >= 0) {
-                        std::vector<std::string> newRows = rows;
-                        int buff = newRows[i][j];
-                        newRows[i][j] = newRows[i][j-1];
-                        newRows[i][j-1] = buff;
-                        std::string rowsString = appendRowsIntoString(newRows);
-                        possibleMovments.push_back(rowsString);
+                        std::vector<std::vector<char>> newMatrix = matrix;
+                        int buff = newMatrix[i][j];
+                        newMatrix[i][j] = newMatrix[i][j-1];
+                        newMatrix[i][j-1] = buff;
+                        std::string strMatrix = getStringFromMatrix(newMatrix);
+                        possibleMovments.push_back(strMatrix);
                     }
                 }
             }
@@ -116,17 +169,19 @@ public:
 
 int main (int argc, char *argv[])
 {
-    //936
-    //417
-    //852
+    auto start = std::chrono::high_resolution_clock::now();
+    //int n, m;
+    //std::cin>>n>>m;
+    //std::string s = "";
+    //for (int i = 0; i < n*m; i++) {
+    //    char c;
+    //    std::cin>>c; 
+    //    s += c;
+    //}
     Solver solv; 
-    for (int k = 0; k < 25; k++) {
-        if(solv.getFound() == true) break;
-        //std::cout<<"k: "<<k<<std::endl;
-        solv.dfs("012534678", 0, k);
-    }
-    std::cout<<"Total de passos: "<<solv.getHistorySize()-1<<std::endl;
-    std::cout<<"Solução:"<<std::endl;
-    solv.printHistory();
+    solv.solve("012534678");
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    std::cout<<"Execution time: "<<duration.count()<<" seconds"<<"\n";
     return 0;
 }
